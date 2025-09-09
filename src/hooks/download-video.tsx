393dbf9611273648fea53cf7fee2c22a323c051a
Markus Sjöqvist <korvkod@gmail.com>
2025-09-09 14:50:56 +0200

    frontend elements added & connected to backend

--- src/hooks/download-video.tsx
+++ src/hooks/download-video.tsx
@@ -1,167 +1,73 @@
 import { Channel, invoke } from "@tauri-apps/api/core";
 import { useEffect, useState } from "react";
-import { YtdlpFormatItem } from "./fetch-video-info";
-import {
-  DownloadEvent,
-  DownloadItem,
-  DownloadSpeed,
-} from "../lib/download-engine";
+import { DownloadEvent } from "../lib/download-engine";
 
+/**
+ * Parameters required to start a video download.
+ */
 interface DownloadParameters {
   url: string;
-  outputDir: string;
-  videoTitle: string;
-  audioFormat: YtdlpFormatItem;
-  videoFormat: YtdlpFormatItem;
 }
 
-export class VideoDownloadItem extends DownloadItem<DownloadParameters> {}
+/**
+ * Represents a video download item with its URL and ongoing status.
+ */
+export class VideoDownloadItem {
+  url: string;
+  ongoing: boolean;
+
+  constructor({ url, ongoing = false }: { url: string; ongoing?: boolean }) {
+    this.url = url;
+    this.ongoing = ongoing;
+  }
+}
 
 type DownloadEventStarted = DownloadEvent<"started", {}>;
-type DownloadEventProgress = DownloadEvent<
-  "progress",
-  {
-    output: string;
-  }
->;
 type DownloadEventFinished = DownloadEvent<"finished", {}>;
+type DownloadEvents = DownloadEventStarted | DownloadEventFinished;
 
-type DownloadEvents =
-  | DownloadEventStarted
-  | DownloadEventProgress
-  | DownloadEventFinished;
-
-function getPercentage(str: string): string | undefined {
-  const result = /\d+(?:\.\d)*\%/.exec(str);
-  return result ? result[0] : undefined;
-}
-
-function getSpeed(str: string): DownloadSpeed | undefined {
-  const result = /(\d+\.\d+)(\w+\/s)/.exec(str);
-  return result ? { rate: parseFloat(result[1]), size: result[2] } : undefined;
-}
-
-function isComplete(str: string): boolean {
-  // When reaching 100%, two outputs will come from backend. One with the progress of the
-  // download, "100.0%", and one with "100%". The second is treated as download complete.
-  return str === "100%";
-}
-
-function createDownloadItem(data: DownloadParameters): VideoDownloadItem {
-  return new DownloadItem({
-    id: data.url,
-    label: data.videoTitle,
-    params: data,
-    children: [
-      new DownloadItem({
-        id: "audio",
-        label: "Audio",
-        size: data.audioFormat.filesize ?? undefined,
-      }),
-      new DownloadItem({
-        id: "video",
-        label: "Video",
-        size: data.audioFormat.filesize ?? undefined,
-      }),
-    ],
-  });
-}
-
+/**
+ * Custom hook to manage video downloads.
+ *
+ * @returns An object which handles a download.
+ */
 export const useDownloadVideo = () => {
-  const [downloadParams, setDownloadParams] = useState(
-    new Map<string, { id: string; params: DownloadParameters }>()
+  const [downloadItem, setDownloadItem] = useState<VideoDownloadItem | null>(
+    null
   );
-  const [items, setItems] = useState(new Map<string, VideoDownloadItem>());
-  const [queue, setQueue] = useState<VideoDownloadItem[]>([]);
 
   useEffect(() => {
-    const _item = queue.at(0);
-    const _params = _item ? downloadParams.get(_item.id) : undefined;
-
-    if (_item && _params) {
+    if (downloadItem instanceof VideoDownloadItem && !downloadItem.ongoing) {
       const channel = new Channel<DownloadEvents>((message) => {
-        if (message.event === "progress") {
-          const output = message.data.output;
-          const percentage = getPercentage(output);
-          const speed = getSpeed(output);
-          let changed = false;
-
-          if (typeof percentage === "string" && !isComplete(percentage)) {
-            const progress = parseFloat(percentage);
-
-            _item.updateProgress(progress);
-            changed = true;
-          }
-
-          if (speed) {
-            _item.speed = speed;
-            changed = true;
-          }
-
-          if (changed) {
-            setItems((prev) => {
-              return new Map(
-                prev.set(_item.id, new DownloadItem({ ..._item }))
-              );
-            });
-            console.log("ITEM:", _item.id, _item.progress, _item.speed);
-          }
+        if (message.event === "started") {
+          console.log("ITEM STARTED");
         } else if (message.event === "finished") {
-          _item.ongoing = false;
-          _item.speed = { rate: 0, size: "" };
-
-          setItems((prev) => {
-            return new Map(prev.set(_item.id, new DownloadItem({ ..._item })));
-          });
-
-          console.log("ITEM DONE:", _item.id, _item.done, _item.progress);
+          console.log("ITEM FINISHED");
+          setDownloadItem(null);
         }
       });
 
       invoke<unknown>("download", {
-        url: _params.params.url,
-        outputDir: _params.params.outputDir,
-        audioFormat: _params.params.audioFormat.format_id,
-        videoFormat: _params.params.videoFormat.format_id,
+        url: downloadItem.url,
         onEvent: channel,
       });
 
-      _item.ongoing = true;
-
-      const newQueue = [...queue];
-      newQueue.shift();
-
-      setQueue(newQueue);
-      setItems((prev) => {
-        return new Map(prev.set(_item.id, new DownloadItem({ ..._item })));
-      });
+      setDownloadItem(
+        new VideoDownloadItem({ ...downloadItem, ongoing: true })
+      );
     }
-
-    console.log("QUEUE:", queue);
-  }, [queue]);
+  }, [downloadItem]);
 
   return {
-    download: (data: DownloadParameters) => {
-      setDownloadParams(
-        (prev) => new Map(prev.set(data.url, { id: data.url, params: data }))
+    startDownload: (data: DownloadParameters) => {
+      setDownloadItem(
+        new VideoDownloadItem({
+          url: data.url,
+          ongoing: false,
+        })
       );
-
-      const item = createDownloadItem(data);
-
-      setItems((prev) => {
-        return new Map(prev.set(item.id, item));
-      });
-
-      setQueue((prev) => {
-        return [...prev, item];
-      });
     },
 
-    getParams: (itemId: string) => {
-      return downloadParams.get(itemId);
-    },
-
-    items,
-    isDownloading: false,
+    downloadItem,
   };
 };
--- src/pages/download-page.tsx
+++ src/pages/download-page.tsx
@@ -1,212 +1,42 @@
-import {
-  Button,
-  ButtonProps,
-  Checkbox,
-  Divider,
-  Input,
-  InputProps,
-  Progress,
-  Select,
-  Selection,
-  SelectItem,
-} from "@heroui/react";
+import { Button, ButtonProps, Input, InputProps } from "@heroui/react";
 import { OneColumnLayout } from "../layouts/one-column";
-import { useCallback, useState } from "react";
-import { useFetchVideoInfo, YtdlpFormatItem } from "../hooks/fetch-video-info";
-import { useDownloadVideo, VideoDownloadItem } from "../hooks/download-video";
-import { open } from "@tauri-apps/plugin-dialog";
+import { useState } from "react";
+
+import { useDownloadVideo } from "../hooks/download-video";
 
 export function DownloadPage() {
   const [url, setUrl] = useState("https://www.youtube.com/watch?v=Dl2vf04UCAM");
-  const { audioFormats, videoFormats, videoTitle, setFetching, fetching } =
-    useFetchVideoInfo(url);
-  const downloader = useDownloadVideo();
-
-  const [selectedAudioFormat, setSelectedAudioFormat] = useState<Selection>(
-    new Set([])
-  );
-  const [selectedVideoFormat, setSelectedVideoFormat] = useState<Selection>(
-    new Set([])
-  );
-  const [preferWorstAudio, setPreferWorstAudio] = useState(true);
-  const [preferWorstVideo, setPreferWorstVideo] = useState(true);
-
-  const [outputDir, setOutputDir] = useState(window.defaultOutputDir);
-
-  const handleFetch = () => {
-    setFetching(true);
-  };
+  const downloadVideo = useDownloadVideo();
 
   const handleDownload = () => {
-    if (
-      (selectedAudioFormat instanceof Set &&
-        selectedAudioFormat.size === 1 &&
-        selectedVideoFormat instanceof Set &&
-        selectedVideoFormat.size === 1) ||
-      true
-    ) {
-      // const audioId = selectedAudioFormat.values().next().value;
-      // const videoId = selectedVideoFormat.values().next().value;
-
-      // if (typeof audioId !== "string") {
-      //   throw Error("Audio format not selected");
-      // }
-
-      // if (typeof videoId !== "string") {
-      //   throw Error("Video format not selected");
-      // }
-
-      // const audioFormat = audioFormats.find((f) => f.id === audioId);
-      // const videoFormat = videoFormats.find((f) => f.id === videoId);
-
-      // if (audioFormat === undefined) {
-      //   throw Error("Could not find audio format info for: " + audioId);
-      // }
-
-      // if (videoFormat === undefined) {
-      //   throw Error("Could not find video format info for: " + videoId);
-      // }
-
-      if (!videoTitle) {
-        throw Error("Video title is not set");
-      }
-
-      if (!outputDir) {
-        throw Error("Output directory is not set");
-      }
-
-      if (preferWorstAudio && preferWorstVideo) {
-        const af = audioFormats.at(0);
-        const vf = videoFormats.at(0);
-
-        if (af) {
-          af.format_id = "wa";
-        }
-
-        if (preferWorstVideo) {
-          if (vf) {
-            vf.format_id = "wv";
-          }
-        }
-
-        if (af && vf) {
-          downloader.download({
-            url,
-            videoTitle,
-            audioFormat: af,
-            videoFormat: vf,
-            outputDir,
-          });
-        }
-      }
-    }
+    downloadVideo.startDownload({ url });
   };
-
-  const onRedownload = useCallback(
-    (itemId: string) => {
-      const item = downloader.items.get(itemId);
-
-      if (item && item.params) {
-        downloader.download(item.params);
-      }
-    },
-    [downloader.items]
-  );
 
   return (
     <OneColumnLayout>
       <UrlInput
         url={url}
         onUrlChange={setUrl}
-        onFetch={handleFetch}
-        isDisabled={downloader.isDownloading || fetching}
+        onDownload={handleDownload}
+        isDisabled={downloadVideo.downloadItem?.ongoing || false}
       />
-      <Divider className="my-6" />
-      <DownloadOptions
-        videoTitle={videoTitle}
-        isDisabled={downloader.isDownloading || fetching}
-        audioFormats={audioFormats}
-        videoFormats={videoFormats}
-        selectedAudioFormat={selectedAudioFormat}
-        selectedVideoFormat={selectedVideoFormat}
-        outputDir={outputDir}
-        onOutputChange={setOutputDir}
-        setSelectedAudioFormat={setSelectedAudioFormat}
-        setSelectedVideoFormat={setSelectedVideoFormat}
-        preferWorstAudio={preferWorstAudio}
-        preferWorstVideo={preferWorstVideo}
-        onPreferWorstAudio={setPreferWorstAudio}
-        onPreferWorstVideo={setPreferWorstVideo}
-      />
-      <Divider className="my-6" />
-      <div className="flex flex-col gap-4">
-        <Button
-          color="primary"
-          className="self-start"
-          onPress={handleDownload}
-          isDisabled={downloader.isDownloading || fetching}
-        >
-          Download
-        </Button>
-        <ProgressList
-          items={Array.from(downloader.items.values())}
-          onRedownload={onRedownload}
-        ></ProgressList>
-      </div>
     </OneColumnLayout>
   );
 }
 
-const ProgressList = ({
-  items,
-  onRedownload,
-}: {
-  items: VideoDownloadItem[];
-  onRedownload: (itemId: string) => void;
-}) => {
-  return (
-    <div className="flex flex-col gap-4">
-      {items.map((item) => {
-        return (
-          <div className="flex flex-col gap-4" key={item.id}>
-            <Progress
-              aria-label={"Download progress bar"}
-              color={"success"}
-              label={" " + item.label}
-              showValueLabel={true}
-              size="md"
-              value={item.progress}
-            />
-            <div className="flex gap-4">
-              <Button
-                onPress={() => {
-                  onRedownload(item.id);
-                }}
-                isDisabled={!item.done}
-              >
-                Re-Download
-              </Button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
-// ===============================================================
-// ===============================================================
-// ===============================================================
-// ===============================================================
-
 interface UrlInputProps {
   url: string;
   isDisabled: boolean;
-  onFetch: ButtonProps["onPress"];
+  onDownload: ButtonProps["onPress"];
   onUrlChange: InputProps["onValueChange"];
 }
 
-const UrlInput = ({ url, isDisabled, onFetch, onUrlChange }: UrlInputProps) => {
+const UrlInput = ({
+  url,
+  isDisabled,
+  onDownload,
+  onUrlChange,
+}: UrlInputProps) => {
   return (
     <div className="text-black flex flex-col gap-4">
       <div className="flex items-center gap-4">
@@ -217,125 +47,10 @@ const UrlInput = ({ url, isDisabled, onFetch, onUrlChange }: UrlInputProps) => {
           onValueChange={onUrlChange}
           isDisabled={isDisabled}
         />
-        <Button color="primary" onPress={onFetch} isDisabled={isDisabled}>
-          Get info
+        <Button color="primary" onPress={onDownload} isDisabled={isDisabled}>
+          Download
         </Button>
       </div>
-    </div>
-  );
-};
-
-// ===============================================================
-// ===============================================================
-// ===============================================================
-// ===============================================================
-
-interface DownloadOptionsProps {
-  isDisabled: boolean;
-  videoFormats: YtdlpFormatItem[];
-  audioFormats: YtdlpFormatItem[];
-  selectedAudioFormat: Selection;
-  selectedVideoFormat: Selection;
-  videoTitle: string;
-  outputDir: string;
-  setSelectedAudioFormat: React.Dispatch<React.SetStateAction<Selection>>;
-  setSelectedVideoFormat: React.Dispatch<React.SetStateAction<Selection>>;
-  onOutputChange: React.Dispatch<React.SetStateAction<string>>;
-  preferWorstAudio: boolean;
-  preferWorstVideo: boolean;
-  onPreferWorstAudio: React.Dispatch<React.SetStateAction<boolean>>;
-  onPreferWorstVideo: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-function makeFormatLabel(item: YtdlpFormatItem): string {
-  return `${item.format_id}\t|\t${item.ext}${
-    item.filesize ? `\t|\t${item.filesize}` : ""
-  }${item.resolution !== "audio only" ? `\t|\t${item.resolution}` : ""}`;
-}
-
-const DownloadOptions = ({
-  isDisabled,
-  videoFormats,
-  audioFormats,
-  selectedAudioFormat,
-  selectedVideoFormat,
-  videoTitle,
-  outputDir,
-  setSelectedAudioFormat,
-  setSelectedVideoFormat,
-  onOutputChange,
-  preferWorstAudio,
-  preferWorstVideo,
-  onPreferWorstAudio,
-  onPreferWorstVideo,
-}: DownloadOptionsProps) => {
-  async function handleOutputDir() {
-    const path = await open({
-      directory: true,
-      multiple: false,
-      title: "Select output directory",
-    });
-
-    if (typeof path === "string") {
-      onOutputChange(path);
-    }
-  }
-
-  const onOutputDirKeyDown: React.KeyboardEventHandler<HTMLInputElement> =
-    useCallback((e) => {
-      if (e.key === "Enter" || e.key === " ") {
-        handleOutputDir();
-      }
-    }, []);
-
-  return (
-    <div className="text-black flex flex-col gap-4">
-      <div>Title: {videoTitle}</div>
-      <Input
-        label="Output Directory"
-        value={outputDir}
-        readOnly
-        onClick={handleOutputDir}
-        onKeyDown={onOutputDirKeyDown}
-      />
-      <Checkbox
-        value="wa"
-        onValueChange={onPreferWorstAudio}
-        isSelected={preferWorstAudio}
-      >
-        Prefer worst audio quality
-      </Checkbox>
-      <Checkbox
-        value="wv"
-        onValueChange={onPreferWorstVideo}
-        isSelected={preferWorstVideo}
-      >
-        Prefer worst video quality
-      </Checkbox>
-      <Select
-        label="Audio"
-        placeholder="Select format"
-        selectedKeys={selectedAudioFormat}
-        onSelectionChange={setSelectedAudioFormat}
-        items={audioFormats}
-        isDisabled={isDisabled}
-      >
-        {(item) => {
-          return <SelectItem>{makeFormatLabel(item)}</SelectItem>;
-        }}
-      </Select>
-      <Select
-        label="Video"
-        placeholder="Select format"
-        selectedKeys={selectedVideoFormat}
-        onSelectionChange={setSelectedVideoFormat}
-        items={videoFormats}
-        isDisabled={isDisabled}
-      >
-        {(item) => {
-          return <SelectItem>{makeFormatLabel(item)}</SelectItem>;
-        }}
-      </Select>
     </div>
   );
 };
